5e28865d36026c0ef2bb5194b9340fe61197ab2e
shuaib shaban <shuaib>
2023-01-14 08:32:43 +0300

    minutesToRead

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,13 +1,27 @@
 import React from "react";
-
-const Article = ({title, date="January 1, 1970", preview}) => {
-    return ( 
-        <article>
-            <h3>{title}</h3>
-            <small>{date}</small>
-            <p>{preview}</p>
-        </article>
-     );
+function minutesToRead(minutes) {
+  if (minutes > 0 && minutes < 30) {
+    const times = Math.ceil(minutes / 5);
+    return "☕️".repeat(times);
+  } else if (minutes >= 30) {
+    const times = Math.ceil(minutes / 10);
+    return "🍱".repeat(times);
+  } else {
+    return "";
+  }
 }
- 
+
+const Article = ({ title, date = "January 1, 1970", preview, minutes }) => {
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>
+        {date} {minutesToRead(minutes)}
+        {minutes} min read
+      </small>
+      <p>{preview}</p>
+    </article>
+  );
+};
+
 export default Article;
